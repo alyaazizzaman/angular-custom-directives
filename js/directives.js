38f88462c4971b71c04d1ebfdc9984155e3fe873
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-01-28 11:47:26 -0700

    practiced using 'restrict' on custom directive

--- js/directives.js
+++ js/directives.js
@@ -1,6 +1,7 @@
 angular.module('CustomDirectiveApp', [])
   .directive('aaYumCheese', function() {
     return {
+      restrict: 'E',
       template: '<img src="http://flexibristol.org/wp-content/uploads/2015/12/cheeses.jpg">'
     };
   });
