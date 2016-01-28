a706812ab4464b098140e938a339aa5e4dae602a
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-01-28 11:17:44 -0700

    created simple custom Angular directive 'aaYumCheese'

---
+++ js/directives.js
@@ -0,0 +1,6 @@
+angular.module('CustomDirectiveApp', [])
+  .directive('aaYumCheese', function() {
+    return {
+      template: '<img src="http://flexibristol.org/wp-content/uploads/2015/12/cheeses.jpg">'
+    };
+  });
